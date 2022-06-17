f87198822a04563d59e32f90fac68b06c562b1d9
Ryosuke NAKATA <ryosuke@RyosukenoMacBook-Pro.local>
2022-06-17 15:52:58 +0900

    fix get

--- server.go
+++ server.go
@@ -12,12 +12,32 @@ func main() {
 	// Set middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-	// Set a root
+	// Set rootings
 	e.GET("/", hello)
+	e.GET("/show", show)
+	e.GET("/users/:name", getUerName)
+	e.POST("/save", save)
 	// Start this server with port1232
-	e.Logger.Fatal(e.Start(":1232"))
+	e.Logger.Fatal(e.Start(":1323"))
 }
 
 func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello Echo!")
+}
+
+func getUerName(c echo.Context) error {
+	name := c.Param("name")
+	return c.String(http.StatusOK, name)
+}
+
+func show(c echo.Context) error {
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "team: "+team+", member: "+member)
+}
+
+func save(c echo.Context) error {
+	name := c.FormValue("name")
+	email := c.FormValue("email")
+	return c.String(http.StatusOK, "name: "+name+", email: "+email)
 }
